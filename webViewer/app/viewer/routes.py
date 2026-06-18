bce455855134a0e01a8699cf30444c8ae59790aa
Matis HADET <matis.hadet@etu.univ-nantes.fr>
2026-06-18 10:54:53 +0200

    clean useless file

--- webViewer/app/viewer/routes.py
+++ webViewer/app/viewer/routes.py
@@ -19,58 +19,9 @@ def base():
 
 @viewer.route('/index')
 def index():
-    navigation = []
 
-    all_pdb_files = PDBFile.get_all_files()
+    return render_template('viewer/index.html', a_variable='Home Page')
 
-    for pdb_file in all_pdb_files:
-        navigation.append({"id": f"{pdb_file.id}", "name": f"{pdb_file.filename}"})
-
-    return render_template('viewer/index.html', a_variable='Index', navigation=navigation)
-
-@viewer.route('/uploadPage')
-def uploadPage():
-    return render_template('viewer/upload.html')
-
-@viewer.route('/pdb_content/<int:file_id>')
-def pdb_content(file_id):
-
-    pdb_file = PDBFile.get_file_by_id(file_id)
-
-    if pdb_file is not None:
-        return Response(pdb_file.content, mimetype='chemical/x-pdb', headers={"Content-Disposition": f"attachment;filename={pdb_file.filename}"})
-    return "Fichier non trouvé", 404
-
-@viewer.route('/uploadFileInDB', methods=['POST'])
-def uploadFile():
-    the_file = request.files['the_file']
-    tech = request.form.get('tech')
-    file_content = the_file.read().decode('utf-8', errors='replace')
-
-    pdb_file = None
-
-    try:
-        pdb_file = PDBFile.save_pdb_file(the_file.filename, file_content)
-    except IntegrityError as e:
-        db.session.rollback()
-        pdb_file = PDBFile.get_file_by_name(the_file.filename)
-        if pdb_file is None:
-            return "Erreur : le fichier existe mais n'a pas pu être récupéré", 500
-
-    return redirect(url_for('viewer.fileInfo', file_id=pdb_file.id, tech=tech))
-
-@viewer.route('/fileInfo/<int:file_id>/<string:tech>')
-def fileInfo(file_id, tech = '3DMol'):
-
-    pdb_file = PDBFile.get_file_by_id(file_id)
-
-    return render_template(
-        'viewer/fileInfo.html',
-        filename=pdb_file.filename,
-        file_content=pdb_file.content,
-        techUsed=tech,
-        pdb_id=pdb_file.id
-    )
 
 @viewer.route('/proteinViewer/<string:tech>', methods=['POST'])
 def proteinViewer(tech = '3DMol'):
@@ -82,17 +33,13 @@ def proteinViewer(tech = '3DMol'):
     fileName = data["file_name"]
     fileContent = data["file_content"]
 
-    #todo regler id et nettoyer le code
-
     return render_template(
-        'viewer/fileInfo.html',
+        'viewer/proteinViewer.html',
         filename=fileName,
         file_content=fileContent,
         techUsed=tech,
         pdb_id=1
     )
-
-
 
 @viewer.route('/fitnessForm', methods=['GET'])
 def fitnessForm():
