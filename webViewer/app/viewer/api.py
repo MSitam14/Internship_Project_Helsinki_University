d764741c81849541ae5a716396940f5014167b1f
Matis HADET <matis.hadet@etu.univ-nantes.fr>
2026-08-03 13:23:32 +0300

    fix name and add todo

--- webViewer/app/viewer/api.py
+++ webViewer/app/viewer/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from ..models import TempSaveScoreOneFile
 
-api = Blueprint('api', __name__, url_prefix='/api')
+api = Blueprint('api', __name__, url_prefix='/api') # add score to the url prefix
 
 # ===== ROUTES API =====
 
