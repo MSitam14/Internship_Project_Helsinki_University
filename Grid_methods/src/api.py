c5ed923cd4f1d8525541171479674bbf748c1b52
Matis HADET <matis.hadet@etu.univ-nantes.fr>
2026-08-18 15:25:21 +0300

    add new api to launch hot spots

--- Grid_methods/src/api.py
+++ Grid_methods/src/api.py
@@ -46,7 +46,7 @@ def _json_safe(value):
 #       }
 @api.route('/comparison', methods=['POST'])
 def calculate_comparison():
-    """POST /api-hot-comp/comparison - calculate hot spot comparison"""
+    """POST /api-hot-comp/comparison - calculate comparison"""
     date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     data = request.get_json()
     if not data:
@@ -99,6 +99,73 @@ def calculate_comparison():
     try: shutil.rmtree(file_path_input)
     except OSError: print(f"Directory '{file_path_input}' not found.")
 
+    return jsonify({
+        'status': 'success',
+        'content': _json_safe(out)
+    })
+
+
+# input: {  
+#           "params": {
+#               "comparison_parameters": {
+#                   "param1":"value1", ...
+#               },
+#               "global_parameters": {
+#                   "param1":"value1", ...
+#               }
+#           }, 
+#           "pdb": {"name": "file1.pdb", "content": "PDB content 1"}, 
+#       }
+@api.route('/hotSpots', methods=['POST'])
+def calculate_hotSpots():
+    """POST /api-hot-comp/hotSpots - calculate hot spot"""
+    date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    data = request.get_json()
+    if not data:
+        return jsonify({
+            'status': 'error',
+            'message': 'No input data provided'
+        }), 400
+    
+    try:
+        params = data['params']
+        pdb = data['pdb']
+    except KeyError as e:
+        return jsonify({
+            'status': 'error',
+            'message': f'Missing parameter: {str(e)}'
+        }), 400
+    
+    file_path_input = 'Grid_methods/data/input/structures/' + date + '/'
+    pdb1_path = file_path_input + pdb["name"]
+    
+    params['hotspot_parameters']["path_to_PDB_directory"] = file_path_input
+    
+    os.makedirs(file_path_input, exist_ok=True)
+    
+    with open(pdb1_path, 'w') as f:
+        f.write(pdb["content"])
+    
+    print(f"\nReceived comparison request for {pdb['name']}")
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    try:
+        out = main_api(params) 
+    except Exception as e:
+        shutil.rmtree(file_path_input)
+        cmd.quit()  # Ensure PyMOL quits to free resources
+        cmd.reinitialize()
+        print(f"Error during hot spots: {str(e)}")
+        return jsonify({
+            'status': 'error',
+            'message': f'Error during hot spots: {str(e)}'
+        }), 400
+    
+    print(f"\nComparison completed for {pdb['name']}. Cleaning up temporary files.")
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
+    
+    try: shutil.rmtree(file_path_input)
+    except OSError: print(f"Directory '{file_path_input}' not found.")
+    
     return jsonify({
         'status': 'success',
         'content': _json_safe(out)
